c31725068b22f85aabd2f39db3d92a782b85cf80
dumaya <alexis.dumay@axa.fr>
2019-10-08 15:04:21 +0200

    Premier commit

---
+++ Ouvrage/src/main/java/dumaya/dev/Ouvrage/OuvrageApplication.java
@@ -0,0 +1,13 @@
+package dumaya.dev.Ouvrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuvrageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuvrageApplication.class, args);
+	}
+
+}
