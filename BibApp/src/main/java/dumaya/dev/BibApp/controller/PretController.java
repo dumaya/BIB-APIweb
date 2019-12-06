d19099ed7e39e422dff8f811c836842848cf555d
Alexis Dumay S627639 <alexis.dumay@axa.fr>
2019-12-06 15:30:21 +0100

    pret en cours OK + log

--- BibApp/src/main/java/dumaya/dev/BibApp/controller/PretController.java
+++ BibApp/src/main/java/dumaya/dev/BibApp/controller/PretController.java
@@ -5,15 +5,22 @@ import dumaya.dev.BibApp.model.Pret;
 import dumaya.dev.BibApp.repository.OuvrageRepository;
 import dumaya.dev.BibApp.repository.PretRepository;
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
+
 
 @RestController
 public class PretController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PretController.class);
+
     @Autowired
     PretRepository pretRepository;
 
@@ -32,11 +39,19 @@ public class PretController {
     }
 
     @GetMapping(value = "/prets/ouvrage/{id}")
-    public Pret pretDeLOuvrage (@PathVariable("id") int id){
+    public Pret pretEnCours (@PathVariable("id") int id){
         List<Pret> listePret = pretRepository.findByIdOuvrage(id);
-
-
-        if (pret == null) throw new NotFoundException("Ce pret n'existe pas");
-        return pret;
+        Pret pretEnCours = null;
+        for (Pret pret : listePret) {
+            if (pret.getDateFin().after(new Date())) {
+                if (pretEnCours != null) {
+                    //cas de 2 prets sur la même période
+                    LOGGER.error("cas de 2 prets sur la même période, idOuvrage : " + pret.getIdOuvrage());
+                } else {
+                    pretEnCours = pret;
+                }
+            }
+        }
+        return pretEnCours;
     }
 }
--- BibApp/src/main/resources/data.sql
+++ BibApp/src/main/resources/data.sql
@@ -2,6 +2,8 @@
 insert into reference (id, titre, auteur) values (1, 'Memoires', 'Hugues POL');
 insert into Ouvrage (id, id_reference, emplacement) values (1, 1, 'Etagere du bas');
 insert into pret (id, id_ouvrage, id_usager,date_fin, top_prolongation) values (1,1,1,'	2020-11-27 00:00:00' ,false);
+insert into pret (id, id_ouvrage, id_usager,date_fin, top_prolongation) values (2,1,3,'	2018-11-27 00:00:00' ,false);
+insert into pret (id, id_ouvrage, id_usager,date_fin, top_prolongation) values (3,1,4,'	2020-12-27 00:00:00' ,false);
 
 insert into reference (id, titre, auteur) values (2, 'Les tracteurs', 'Sesmat');
 insert into Ouvrage (id, id_reference, emplacement) values (2, 2, '5eme couloir, 5eme ligne');
